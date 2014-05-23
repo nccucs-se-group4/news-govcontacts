e80c09d1efcb40a322ebd6f1de0986b6090ae314
yuanhh <yuan415030@gmail.com>
2014-05-23 22:40:18 +0800

    Update detectBrowser.js

--- javascripts/detectBrowser.js
+++ javascripts/detectBrowser.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
-    if (bowser.msie && bowser.version <= 6) {
-        alert('Hello China');
-    } else if (bowser.firefox){
-        alert('Hello Foxy');
-    } else if (bowser.chrome){
-        alert('Hello Silicon Valley');
-    } else if (bowser.safari){
-        alert('Hello Apple Fan');
-    } else if(bowser.iphone || bowser.android){
-        alert('Hello mobile');
-    }
-</script>
+if (bowser.msie && bowser.version <= 6) {
+    alert('Hello China');
+} else if (bowser.firefox){
+    alert('Hello Foxy');
+} else if (bowser.chrome){
+    alert('Hello Silicon Valley');
+} else if (bowser.safari){
+    alert('Hello Apple Fan');
+} else if(bowser.iphone || bowser.android){
+    alert('Hello mobile');
+}
