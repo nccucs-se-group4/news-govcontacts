6c73ae1db05068b3acd29be5323a7c33e4926e90
yuanhh <yuan415030@gmail.com>
2014-06-22 20:20:09 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -2,20 +2,12 @@ function detectBrowser(){
 	var chromeDownloader = "https://github.com/nccucs-se-group4/news-LyContacts-extension";
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
-	var oBrowser = new detectBrowser();
-	var sAgent = navigator.userAgent.toLowerCase();
-	this.isIE = (sAgent.indexOf("msie")!=-1); //IE6.0-7
-	this.isFF = (sAgent.indexOf("firefox")!=-1);//firefox
-	this.isSa = (sAgent.indexOf("safari")!=-1);//safari
-	this.isOp = (sAgent.indexOf("opera")!=-1);//opera
-	this.isNN = (sAgent.indexOf("netscape")!=-1);//netscape
-	this.isCh = (sAgent.indexOf("chrome")!=-1);//chrome
-	this.isMa = this.isIE;//marthon
-	this.isOther = (!this.isIE && !this.isFF && !this.isSa && !this.isOp && !this.isNN && !this.isSa);//unknown Browser
-	if (this.isCh)
- 		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
-	else if (this.isFF)
- 		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
-	else if (this.isSa)
- 		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
+	if (navigator.userAgent.indexOf("Firefox")!=-1)
+		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
+	if (navigator.userAgent.indexOf("MSIE")!=-1)
+		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
+	if (navigator.userAgent.indexOf("Chrome")!=-1)
+		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
+	if (navigator.userAgent.indexOf("Safari")!=-1)
+		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
 }
