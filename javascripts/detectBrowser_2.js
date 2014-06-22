1be6bfe7cd29bcc865264b057b3fa275777cbac1
yuanhh <yuan415030@gmail.com>
2014-06-22 22:01:23 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -2,45 +2,37 @@ function detectBrowser(){
 	var chromeDownloader = "https://github.com/nccucs-se-group4/news-LyContacts-extension";
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
-var objappVersion = navigator.appVersion;
-var objAgent = navigator.userAgent;
-var objbrowserName  = navigator.appName;
-var objfullVersion  = ''+parseFloat(navigator.appVersion); 
-var objBrMajorVersion = parseInt(navigator.appVersion,10);
-var objOffsetName,objOffsetVersion,ix;
+	var objappVersion = navigator.appVersion;
+	var objAgent = navigator.userAgent;
+	var objbrowserName  = navigator.appName;
+	var objfullVersion  = ''+parseFloat(navigator.appVersion); 
+	var objBrMajorVersion = parseInt(navigator.appVersion,10);
+	var objOffsetName,objOffsetVersion,ix;
 
-// In Chrome 
-if ((objOffsetVersion=objAgent.indexOf("Chrome"))!=-1) {
- objbrowserName = "Chrome";
- objfullVersion = objAgent.substring(objOffsetVersion+7);
-}
-// In Microsoft internet explorer
-else if ((objOffsetVersion=objAgent.indexOf("MSIE"))!=-1) {
- objbrowserName = "Microsoft Internet Explorer";
- objfullVersion = objAgent.substring(objOffsetVersion+5);
-}
-
-// In Firefox
-else if ((objOffsetVersion=objAgent.indexOf("Firefox"))!=-1) {
- objbrowserName = "Firefox";
-}
-// In Safari 
-else if ((objOffsetVersion=objAgent.indexOf("Safari"))!=-1) {
- objbrowserName = "Safari";
- objfullVersion = objAgent.substring(objOffsetVersion+7);
- if ((objOffsetVersion=objAgent.indexOf("Version"))!=-1) 
-   objfullVersion = objAgent.substring(objOffsetVersion+8);
-}
-// For other browser "name/version" is at the end of userAgent 
-else if ( (objOffsetName=objAgent.lastIndexOf(' ')+1) < 
-          (objOffsetVersion=objAgent.lastIndexOf('/')) ) 
-{
- objbrowserName = objAgent.substring(objOffsetName,objOffsetVersion);
- objfullVersion = objAgent.substring(objOffsetVersion+1);
- if (objbrowserName.toLowerCase()==objbrowserName.toUpperCase()) {
-  objbrowserName = navigator.appName;
- }
-}
+	// In Chrome 
+	if ((objOffsetVersion=objAgent.indexOf("Chrome"))!=-1) {
+ 		objbrowserName = "Chrome";
+ 		objfullVersion = objAgent.substring(objOffsetVersion+7);
+	}
+	// In Firefox
+	else if ((objOffsetVersion=objAgent.indexOf("Firefox"))!=-1) {
+ 		objbrowserName = "Firefox";
+	}
+	// In Safari 
+	else if ((objOffsetVersion=objAgent.indexOf("Safari"))!=-1) {
+ 		objbrowserName = "Safari";
+ 		objfullVersion = objAgent.substring(objOffsetVersion+7);
+ 		if ((objOffsetVersion=objAgent.indexOf("Version"))!=-1) 
+   			objfullVersion = objAgent.substring(objOffsetVersion+8);
+	}
+	// For other browser "name/version" is at the end of userAgent 
+	else if ( (objOffsetName=objAgent.lastIndexOf(' ')+1) < 
+        	(objOffsetVersion=objAgent.lastIndexOf('/')) ) {
+		objbrowserName = objAgent.substring(objOffsetName,objOffsetVersion);
+		objfullVersion = objAgent.substring(objOffsetVersion+1);
+		if (objbrowserName.toLowerCase()==objbrowserName.toUpperCase()) {
+			objbrowserName = navigator.appName;
+ 	}
 // trimming the fullVersion string at semicolon/space if present
 if ((ix=objfullVersion.indexOf(";"))!=-1)
    objfullVersion=objfullVersion.substring(0,ix);
