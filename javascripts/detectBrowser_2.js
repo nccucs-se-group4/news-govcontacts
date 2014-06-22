2db5ac3dd6915f21cbea5f53cdfecd3f4ccf69cc
yuanhh <yuan415030@gmail.com>
2014-06-22 21:45:55 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -3,8 +3,10 @@ function detectBrowser(){
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
 	if (navigator.userAgent.indexOf("Firefox")!=-1)
-		text = "<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>"
+		document.open()
+		ocument.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
 		document.body.appendChild(text);
+		document.close()
 	if (navigator.userAgent.indexOf("Chrome")!=-1)
 		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
 	else if (navigator.userAgent.indexOf("Safari")!=-1)
