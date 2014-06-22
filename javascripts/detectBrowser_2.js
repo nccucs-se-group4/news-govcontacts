4ff53a5d0f93fa3f8a04824ec07c3416b969db62
yuanhh <yuan415030@gmail.com>
2014-06-22 20:16:20 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -3,19 +3,19 @@ function detectBrowser(){
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
 	var oBrowser = new detectBrowser();
-	if (oBrowser.isIE) { 
-		alert("IE6.0/7.0(or above version)."); 
-	} 
-	if (oBrowser.isSa && !oBrowser.isCh) { 
-		alert("Safari."); 
-	} 
-	if (oBrowser.isOp) { 
-		alert("Opera."); 
-	} 
-	if (oBrowser.isCh && oBrowser.isSa) { 
-		alert("Chrom."); 
-	} 
-	if(oBrowser.isFF) { 
-		alert("FireFox."); 
-	}
+	var sAgent = navigator.userAgent.toLowerCase();
+	this.isIE = (sAgent.indexOf("msie")!=-1); //IE6.0-7
+	this.isFF = (sAgent.indexOf("firefox")!=-1);//firefox
+	this.isSa = (sAgent.indexOf("safari")!=-1);//safari
+	this.isOp = (sAgent.indexOf("opera")!=-1);//opera
+	this.isNN = (sAgent.indexOf("netscape")!=-1);//netscape
+	this.isCh = (sAgent.indexOf("chrome")!=-1);//chrome
+	this.isMa = this.isIE;//marthon
+	this.isOther = (!this.isIE && !this.isFF && !this.isSa && !this.isOp && !this.isNN && !this.isSa);//unknown Browser
+	if (this.isCh)
+ 		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
+	else if (this.isFF)
+ 		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
+	else if (this.isSa)
+ 		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
 }
