58c8f9fed1c1112091bfb3a56eb8a4ab74354fbd
yuanhh <yuan415030@gmail.com>
2014-06-22 19:44:10 +0800

    Create detectBrowser_2.js

---
+++ javascripts/detectBrowser_2.js
@@ -0,0 +1,15 @@
+function detectBrowser(){
+	var sAgent = navigator.userAgent.toLowerCase();
+	this.isIE = (sAgent.indexOf("msie")!=-1); //IE6.0-7
+	this.isFF = (sAgent.indexOf("firefox")!=-1);//firefox
+	this.isSa = (sAgent.indexOf("safari")!=-1);//safari
+	this.isOp = (sAgent.indexOf("opera")!=-1);//opera
+	this.isNN = (sAgent.indexOf("netscape")!=-1);//netscape
+	this.isCh = (sAgent.indexOf("chrome")!=-1);//chrome
+	if (this.isFF)
+  document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
+  else if (this.isCh)
+  document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
+  else if (this.isSa)
+  document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
+}
