1a0375e6ddf005545aa57bab32dc542eaf4094e3
yuanhh <yuan415030@gmail.com>
2014-06-22 19:56:00 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -2,20 +2,20 @@ function detectBrowser(){
 	var chromeDownloader = "https://github.com/nccucs-se-group4/news-LyContacts-extension";
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
-	var sAgent = navigator.userAgent.toLowerCase();
-	this.isIE = (sAgent.indexOf("msie")!=-1); //IE6.0-7
-	this.isFF = (sAgent.indexOf("firefox")!=-1);//firefox
-	this.isSa = (sAgent.indexOf("safari")!=-1);//safari
-	this.isOp = (sAgent.indexOf("opera")!=-1);//opera
-	this.isNN = (sAgent.indexOf("netscape")!=-1);//netscape
-	this.isCh = (sAgent.indexOf("chrome")!=-1);//chrome
-	var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-	if (is.firefox)
-  		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
-  	else if (this.isCh)
-  		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
-  	else if (this.isSa)
-  		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
-  	else
-  		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
+	var oBrowser = new detectBrowser();
+	if (oBrowser.isIE) { 
+		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>"); 
+	} 
+	if (oBrowser.isSa && !oBrowser.isCh) { 
+		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>"); 
+	} 
+	if (oBrowser.isOp) { 
+		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>"); 
+	} 
+	if (oBrowser.isCh && oBrowser.isSa) { 
+		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
+	} 
+	if(oBrowser.isFF) { 
+		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>"); 
+	}
 }
