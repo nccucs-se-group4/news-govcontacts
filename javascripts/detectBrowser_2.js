2f152730c7f9e4642101fd04ba370ae9af9be491
yuanhh <yuan415030@gmail.com>
2014-06-22 20:14:00 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -2,18 +2,20 @@ function detectBrowser(){
 	var chromeDownloader = "https://github.com/nccucs-se-group4/news-LyContacts-extension";
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
-	var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-    	// Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
-	var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
-	var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-    	// At least Safari 3+: "[object HTMLElementConstructor]"
-	var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
-	var isIE = /*@cc_on!@*/false || !!document.documentMode;   // At least IE6
-
-	if (isChrome)
-		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
-	if (isForefox)
-		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
-	if (isSafari)
-		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
+	var oBrowser = new detectBrowser();
+	if (oBrowser.isIE) { 
+		alert("IE6.0/7.0(or above version)."); 
+	} 
+	if (oBrowser.isSa && !oBrowser.isCh) { 
+		alert("Safari."); 
+	} 
+	if (oBrowser.isOp) { 
+		alert("Opera."); 
+	} 
+	if (oBrowser.isCh && oBrowser.isSa) { 
+		alert("Chrom."); 
+	} 
+	if(oBrowser.isFF) { 
+		alert("FireFox."); 
+	}
 }
