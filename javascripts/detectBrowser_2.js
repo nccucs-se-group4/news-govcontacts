04079520422a604fec0391684eaa4c0319767d9f
yuanhh <yuan415030@gmail.com>
2014-06-22 21:35:27 +0800

    Update detectBrowser_2.js

--- javascripts/detectBrowser_2.js
+++ javascripts/detectBrowser_2.js
@@ -3,10 +3,9 @@ function detectBrowser(){
 	var firfoxDownloader = "https://www.dropbox.com/s/mbvi0icx8ejlkf4/firefox-extension.xpi?dl=1";
 	var safariDownloader = "https://www.dropbox.com/s/pvhgrkqhbent6th/VisitedLinksLogger.safariextz?dl=1";
 	if (navigator.userAgent.indexOf("Firefox")!=-1)
-		document.open()
-		document.write("<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>");
-		document.close()
-	else if (navigator.userAgent.indexOf("Chrome")!=-1)
+		text = "<a href=" + firefoxDownloader + " class=\"xbutton\"> Firefox </a>"
+		document.body.appendChild(text);
+	if (navigator.userAgent.indexOf("Chrome")!=-1)
 		document.write("<a href=" + chromeDownloader + " class=\"xbutton\"> Chrome </a>");
 	else if (navigator.userAgent.indexOf("Safari")!=-1)
 		document.write("<a href=" + safariDownloader + " class=\"xbutton\"> Safari </a>");
